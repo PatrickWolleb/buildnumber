f21f996d1409bb33e482d4cf5abf00733cbe26d7
Patrick Wolleb <patrick.wolleb@shmedia.co.uk>
2014-10-16 03:02:21 +0100

    Adds exploration code that is not working

--- server.go
+++ server.go
@@ -1,20 +1,102 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+  "github.com/go-martini/martini"
+  "github.com/codegangsta/martini-contrib/binding"
+  "github.com/martini-contrib/render"
+  "labix.org/v2/mgo"
+  "labix.org/v2/mgo/bson"
 )
 
-func main() {
-    http.HandleFunc("/", hello)
-    fmt.Println("listening...")
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+
+
+// Application refering to a Werker App
+type Application struct {
+    Id          bson.ObjectId   `bson:"_id"`
+    Name        string          `bson:"name"`
+    Number      int             `bson:"number"`
+}
+
+
+
+
+// DB Returns a martini.Handler
+func DB() martini.Handler {
+    session, err := mgo.Dial("mongodb://localhost")
     if err != nil {
-      panic(err)
+        panic(err)
+    }
+
+    return func(c martini.Context) {
+        s := session.Clone()
+        c.Map(s.DB("buildnumber"))
+        defer s.Close()
+        c.Next()
     }
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
+
+
+func main() {
+  m := martini.Classic()
+  
+  m.Use(render.Renderer())
+  m.Use(DB())
+
+
+  m.Get("/", func() string {
+    return "Welcome to build number server";
+  })
+  m.Get("/api", func(r render.Render) {
+     r.JSON(200, map[string]interface{}{"message": "No direct access"})
+  })
+
+
+
+  // Lists applications
+  m.Get("/api/applications", func(r render.Render, db *mgo.Database) {
+    var apps []Application
+    db.C("applications").Find(nil).All(&apps)
+    r.JSON(200, apps)
+  })
+
+
+  //Creates application
+  m.Post("/api/applications", 
+    binding.Bind(Application{}), 
+    func(r render.Render, db *mgo.Database, app Application) {
+      //app.Number = 0
+      db.C("applications").Insert(&app)
+      r.JSON(201, app)
+  })
+
+
+   //Creates new build on application
+  m.Post("/api/applications/:id/inc", 
+    func(r render.Render, db *mgo.Database, params martini.Params) {
+    
+   
+
+
+    var app Application
+    
+    change := mgo.Change{
+        Update: bson.M{"$inc": bson.M{"number": 1}},
+        ReturnNew: true,
+    }
+    
+    db.C("applications").FindId(bson.ObjectIdHex(params["id"])).Apply(change,&app)  
+
+
+
+
+    //return db.C("applications").Find(bson.M{"_id": "543f0ba9cdaa667d250a62db"}).Count();
+    r.JSON(200, app)
+  })
+
+
+
+  m.Run()
+
+   
 }
